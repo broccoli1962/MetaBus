134129e9f33e0109b1f9cb498f149cf549a1454b
broccoli1962 <aq0011@naver.com>
2025-07-24 21:42:17 +0900

    [ feat ] 게임씬 전환 트리거 제작
    [ feat ] 게임씬 전환 트리거 제작

--- Assets/Scripts/Entity/PlayerController.cs
+++ Assets/Scripts/Entity/PlayerController.cs
@@ -1,14 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.SearchService;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : BaseController
 {
     private Camera cam;
+    private string gameSceneText;
+    private bool inRange = false;
 
     protected override void Start()
     {
+        base.Start();
         cam = Camera.main;
     }
 
@@ -17,5 +22,21 @@ public class PlayerController : BaseController
     {
         movementDirection = inputValue.Get<Vector2>();
         movementDirection = movementDirection.normalized;
+    }
+
+    void OnInteract(InputValue inputValue) {
+        if (inRange) {
+            SceneManager.LoadScene(gameSceneText);
+        }
+        else
+        {
+            Debug.LogError($"Not Found {gameSceneText} Scene");
+        }
+    }
+
+    public void ChangeGameScene(string gameText, bool inRange)
+    {
+        this.inRange = inRange;
+        gameSceneText = gameText;
     }
 }
